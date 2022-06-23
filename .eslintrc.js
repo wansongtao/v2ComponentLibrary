7fb7e3da1466166563365ca98b6c5ffde0dca004
wansongtao <18101837209@163.com>
2022-06-23 11:01:32 +0800

    ci:禁用eslint，使用默认的prettier插件格式化代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,5 @@ module.exports = {
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+  rules: 0,
 };
