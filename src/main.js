dc9c4e8b3a3ff3ab49a405d198f59dadee36acdc
wansongtao <18101837209@163.com>
2022-06-24 15:36:17 +0800

    build:引入@vue/composition-api库

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import './permission'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './permission';
+import VueCompositionAPI from '@vue/composition-api';
+
+Vue.use(VueCompositionAPI);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app');
