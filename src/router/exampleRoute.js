25d3019ec51c62557c176c1345a5c136e44604c4
wansongtao <18101837209@163.com>
2022-06-24 16:19:04 +0800

    feat:添加文件选择器使用示例

--- src/router/exampleRoute.js
+++ src/router/exampleRoute.js
@@ -6,6 +6,14 @@ const exampleRoutes = [
     meta: {
       title: '页面元素转图片'
     }
+  },
+  {
+    path: 'file',
+    name: 'file',
+    component: () => import('@/views/fileExample/index.vue'),
+    meta: {
+      title: '文件选择器'
+    }
   }
 ];
 
---
+++ src/views/fileExample/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="file_example">
+    <FileSelector
+      ref="fileCom"
+      accept="image/*"
+      :multiple="false"
+      :drag="true"
+      @filechange="onChangeFile"
+    />
+  </div>
+</template>
+
+<script>
+import FileSelector from '@/components/FileSelector/index.vue';
+
+export default {
+  name: 'fileExample',
+  components: {
+    FileSelector
+  },
+  data() {
+    return {
+      fileCom: null
+    }
+  },
+  methods: {
+    onChangeFile(files) {
+      console.log(files);
+      console.log(this.$refs.fileCom);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
